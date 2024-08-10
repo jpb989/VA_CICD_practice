b08ccc2749fb986f571c64fc3cb6a5e2f68969c2
jpb989 <joshpaulbakugaan@gmail.com>
2024-08-11 03:18:44 +0530

    added ci/cd and flake8

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(null=False, blank=False, max_length=255)
+    author = models.CharField(null=False, blank=False, max_length=255)
+    slug = models.SlugField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.title
